98620bb154b8a9d97d64e9fdbbcce78e5b3a293f
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2019-08-18 23:12:55 +0100

    removing test fields

--- src/app/client/input/input.component.ts
+++ src/app/client/input/input.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-input',
-  templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+    selector: 'app-input',
+    templateUrl: './input.component.html',
+    styleUrls: ['./input.component.scss']
 })
 export class InputComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    sendToServer() {
+
+    }
 
 }
