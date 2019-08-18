0e82cf87eefa6c9035da48a01c3499330c45cb58
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2019-08-18 14:55:02 +0100

    web client start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
+import * as signalR from '@aspnet/signalr';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    encapsulation: ViewEncapsulation.None,
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ArchaicQuestII-Client';
+export class AppComponent implements OnInit {
+    title = 'ArchaicQuestII-Client';
+    connection: any;
+    appendMessage: string;
+
+    @ViewChild('input', { static: true }) input: ElementRef;
+    @ViewChild('btn', { static: true }) btn: ElementRef;
+    ngOnInit(): void {
+        this.connection = new signalR.HubConnection('http://localhost:57814/Hubs/game');
+        this.connection.on('SendMessage', (sender, message) => {
+            this.appendMessage += sender + ': ' + message;
+        });
+
+        this.connection.on('SendAction', (sender, action) => {
+            this.appendMessage += sender + ' ' + action;
+        });
+
+        this.connection.start().then(() => {
+            this.input.nativeElement.disabled = false;
+            this.btn.nativeElement.disabled = false;
+        });
+    }
+
+
+    send() {
+        this.connection.send('Send', this.input.nativeElement.value);
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientComponent } from './client/client.component';
+import { WindowComponent } from './client/window/window.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ClientComponent,
+        WindowComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
