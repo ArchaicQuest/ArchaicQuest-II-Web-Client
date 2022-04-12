b6a40786376dabba872099c122258b72d910b544
LiamKenneth <liam.kenneth@hotmail.co.uk>
2022-04-13 00:12:03 +0100

    added stats to the char screen

--- src/app/player/Interface/player.interface.ts
+++ src/app/player/Interface/player.interface.ts
@@ -1,59 +1,60 @@
 export interface Player {
-    accountId: string;
-    id?: string;
-    name: string;
-    gender: string;
-    race: string;
-    className: string;
-    inventory: any[];
-    equipped: any;
-    level: string;
-    description: string;
-    alignmentScore: number;
-    totalExperience?: number;
-    experience?: number;
-    experienceToNextLevel?: number;
-    stats: {
-        hitPoints: number;
-        manaPoints: number;
-        movePoints: number;
-    };
-    maxStats: {
-        hitPoints: number;
-        manaPoints: number;
-        movePoints: number;
-    };
-    attributes: any;
-    maxAttributes: any;
-    targetName?: string;
-    armorRating: {
-        armour: number;
-        magic: number;
-    };
-    status: string;
-    money?: {
-        gold: number;
-        silver: number;
-        copper: number;
-    };
-    qffects?: any;
-    weight: number,
-    MobKills?: number,
-    Trains?: number,
-    MobDeaths?: number,
-    QuestPoints?: number,
-    Practices?: number,
-    PlayerKills?: number,
-    config?: any;
-    joinedDate?: Date;
-    LastLoginTime?: Date;
-    LastCommandTime?: Date;
-    Build: string,
-    Face: string,
-    Skin: string,
-    Eyes: string,
-    FacialHair: string,
-    HairColour: string,
-    HairLength: string,
-    HairTexture: string,
+  accountId: string;
+  id?: string;
+  name: string;
+  gender: string;
+  race: string;
+  playTime?: number;
+  className: string;
+  inventory: any[];
+  equipped: any;
+  level: string;
+  description: string;
+  alignmentScore: number;
+  totalExperience?: number;
+  experience?: number;
+  experienceToNextLevel?: number;
+  stats: {
+    hitPoints: number;
+    manaPoints: number;
+    movePoints: number;
+  };
+  maxStats: {
+    hitPoints: number;
+    manaPoints: number;
+    movePoints: number;
+  };
+  attributes: any;
+  maxAttributes: any;
+  targetName?: string;
+  armorRating: {
+    armour: number;
+    magic: number;
+  };
+  status: string;
+  money?: {
+    gold: number;
+    silver: number;
+    copper: number;
+  };
+  qffects?: any;
+  weight: number,
+  MobKills?: number,
+  Trains?: number,
+  MobDeaths?: number,
+  QuestPoints?: number,
+  Practices?: number,
+  PlayerKills?: number,
+  config?: any;
+  joinedDate?: Date;
+  LastLoginTime?: Date;
+  LastCommandTime?: Date;
+  Build: string,
+  Face: string,
+  Skin: string,
+  Eyes: string,
+  FacialHair: string,
+  HairColour: string,
+  HairLength: string,
+  HairTexture: string,
 }
--- src/app/player/manage/manage.component.ts
+++ src/app/player/manage/manage.component.ts
@@ -14,6 +14,10 @@ export class ManageCharactersComponent implements OnInit {
   characters: Player[] = [];
   accountId: string;
   isLoading = true;
+  totalPlayingHours = 0;
+  totalKills = 0;
+  totalDeaths = 0;
+  additednessRating = "";
 
   constructor(private _service: ManageCharactersService, private _router: Router, private _toast: ToastrService) {
   }
@@ -39,6 +43,19 @@ export class ManageCharactersComponent implements OnInit {
           new Date(b.LastLoginTime).getTime() - new Date(a.LastLoginTime).getTime()
         );
 
+        this.characters.forEach(char => {
+
+          this.totalPlayingHours += char['PlayTime'];
+          this.totalKills += char.MobKills;
+          this.totalDeaths += char.MobDeaths;
+
+
+        });
+
+        this.totalPlayingHours = Math.floor(this.totalPlayingHours / 60);
+        this.setAddictedText(this.totalPlayingHours)
+        console.log(this.totalPlayingHours)
+
 
 
         this.isLoading = false;
@@ -59,6 +76,73 @@ export class ManageCharactersComponent implements OnInit {
 
   PlayCharacter(id: string) {
     this._service.PlayCharacter(id);
+  }
+
+  setAddictedText(hours: number) {
+    if (hours < 15) {
+      this.additednessRating = 'Neophyte, learning the ropes.'
+    }
+    else
+      if (hours < 30) {
+        this.additednessRating = 'I\'m starting to get into this...'
+      }
+      else
+        if (hours < 45) {
+          this.additednessRating = 'A week is long time in the realm of ArchaicQuest.'
+        }
+        else
+          if (hours < 60) {
+            this.additednessRating = 'Just one more level.'
+          }
+          else
+            if (hours < 75) {
+              this.additednessRating = 'Repetitive Strain Injury anyone?'
+            }
+            else
+              if (hours < 90) {
+                this.additednessRating = 'Remember to call your work and ask for more sick leave.'
+              }
+              else
+                if (hours < 105) {
+                  this.additednessRating = 'Remember, eating gives you the strength to keep on playing.'
+                }
+                else
+                  if (hours < 120) {
+                    this.additednessRating = 'No-one can accuse you of a lacking commitment.'
+                  }
+                  else
+                    if (hours < 135) {
+                      this.additednessRating = 'Time to change your underwear.'
+                    }
+                    else
+                      if (hours < 150) {
+                        this.additednessRating = 'It\'s not really addictive, I just can\'t stop playing...'
+                      }
+                      else
+                        if (hours < 180) {
+                          this.additednessRating = 'Sleeping is for wimps.'
+                        }
+                        else
+                          if (hours < 195) {
+                            this.additednessRating = 'Time to order another takeaway pizza.'
+                          }
+                          else
+                            if (hours < 210) {
+                              this.additednessRating = 'Your Relationship has expired.'
+                            }
+                            else
+                              if (hours < 225) {
+                                this.additednessRating = 'What are humans?'
+                              }
+                              else
+                                if (hours < 240) {
+                                  this.additednessRating = 'Turning your underwear inside out saves on washing.'
+                                }
+                                else
+                                  if (hours < 255) {
+                                    this.additednessRating = 'I can give up this game whenever i like. Just dont want to yet...'
+                                  }
+
   }
 
 
